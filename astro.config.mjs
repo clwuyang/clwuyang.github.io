6aed1ca659bfea7134f58b1341c26b19a673252e
Chenglong Wu Yang <cwuyang@hawk.iit.edu>
2024-09-03 19:53:05 -0500

    Redesign whole website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-
-import robotsTxt from "astro-robots-txt"
-
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import robotsTxt from 'astro-robots-txt';
+import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [
+    tailwind(),
+    robotsTxt(),
+    react()  // Adding React integration
+  ],
   site: 'https://clwuyang.github.io',
-})
+});
